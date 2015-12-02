1497ee9116238291c65233d7b34fa484ec99fdf3
senarios <zyz0205@hotmail.com>
2015-12-02 20:25:58 +0800

    latest version on 12/2/2015

--- ClassLibrary1/Master_Control.cs
+++ ClassLibrary1/Master_Control.cs
@@ -18,7 +18,7 @@ using Windows.UI.Xaml.Navigation;
 using System.Threading;
 using getBing;
 using Wordlist.DB;
-using App1;
+using UI;
 
 
 public enum PARTICIPANT
@@ -26,7 +26,8 @@ public enum PARTICIPANT
     P_M,
     M_U,
     M_S,
-    M_D
+    M_D,
+    U_M
 }
 
 /// <summary>
@@ -57,8 +58,21 @@ public enum METHOD_P_M
    method_click_event,
    method_others
 }
-
-
+public enum METHOD_U_M
+{
+    method_add_to_vocabularybook,
+    method_delete_from_vocabularybook,
+    method_others
+}
+public struct IN_MESSAGE
+{
+    public String wordbook_str;
+    public String word_str;
+}
+public struct OUT_MESSAGE
+{
+    public String info;
+}
 public struct MESSAGE
 {
     public PARTICIPANT parti;
@@ -66,11 +80,10 @@ public struct MESSAGE
     public METHOD_M_D method_m_d;
     public METHOD_P_M method_p_m;
     public METHOD_M_U method_m_u;
+    public METHOD_U_M method_u_m;
 
-    public String in_message;
-    public String out_message;
-    public String wordbook_str;
-    public String word_str;
+    public IN_MESSAGE in_message;
+    public OUT_MESSAGE out_message;
 }
 namespace MASTER_CONTROLL
 {
@@ -78,7 +91,7 @@ namespace MASTER_CONTROLL
     public interface  COMUNICATION_Interface
     {
         MESSAGE message { get; set; }
-       Task<string> send_messageAsync();
+        void send_messageAsync();
 	    //bool receive_message();
        
     }
@@ -98,9 +111,70 @@ namespace MASTER_CONTROLL
                 message_ = value;
             }
         }
-        public async Task<string> send_messageAsync()
+        public async void send_messageAsync()
         {
-          
+            /*in case that master control is called, first transferring intension to relative call action*/
+            switch (message_.parti)
+            {
+                case PARTICIPANT.U_M:
+                    switch (message_.method_u_m)
+                    {
+                        case METHOD_U_M.method_add_to_vocabularybook:
+                            message_.parti = PARTICIPANT.M_S;
+                            message_.method_m_s = METHOD_M_S.method_add_word;
+                            break;
+                        case METHOD_U_M.method_delete_from_vocabularybook:
+                            message_.parti = PARTICIPANT.M_S;
+                            message_.method_m_s = METHOD_M_S.method_delete_word;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+
+                case PARTICIPANT.P_M:
+                    switch (message_.method_p_m)
+                    {
+                        case METHOD_P_M.method_click_event:
+                            //check whether in data base
+                            WordListDB wordlistdb_for_PDF = new WordListDB();
+                            string result_definition = await wordlistdb_for_PDF.GetWordDefinitionInWordBook(message_.in_message.wordbook_str, message_.in_message.word_str);
+                            
+                            //if word is in the data base
+                            if (result_definition.Length > 2)
+                            {
+                                var result_definition_temp = new StringBuilder(result_definition);
+                                result_definition_temp[1] = '1';
+                                result_definition =  result_definition_temp.ToString();
+                                //shift to M_U
+                                message_.parti = PARTICIPANT.M_U;
+                                message_.method_m_u = METHOD_M_U.method_show_definition;
+                                message_.in_message.word_str = result_definition;
+                                message_.in_message.wordbook_str = "default";
+                                
+                            }
+                            else
+                            {
+                                //bing look up
+                                //then shift to M_U
+                                message_.parti = PARTICIPANT.M_U;
+                                message_.method_m_u = METHOD_M_U.method_show_definition;
+                                message_.in_message.word_str = result_definition;
+                                message_.in_message.wordbook_str = "default";
+                            }
+                           
+
+                            break; //result_show;
+                        default:
+                            break; //"method without definition";
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////
             switch (this.message_.parti)
             {
                 // LIN JIANPING implementation, communication between master and bing.
@@ -108,33 +182,36 @@ namespace MASTER_CONTROLL
                     switch (this.message_.method_m_d)
                     {
                         case METHOD_M_D.method_lookup:
-                            message_.out_message = await LexiconQuery.GetLexicon(message_.in_message);
-                            return "true";
+                            message_.out_message.info = await LexiconQuery.GetLexicon(message_.in_message.word_str);
+                            return; //"true";
                             
                         case METHOD_M_D.method_others:
-                            return "true";
-                        default: return "method without definition";
+                            return; //"true";
+                        default: return; //"method without definition";
                             
                     }
                     
                 //communication between Master and data base
                 case PARTICIPANT.M_S:
                     //instance object
-                    WordListDB wordlistdb = WordListDB.instance_;
+                    WordListDB wordlistdb = new WordListDB();
                     switch (this.message_.method_m_s)
                     {
                         
                         case METHOD_M_S.method_add_word:
-                           bool result_add = await wordlistdb.AddWordToWordBook(message_.wordbook_str, message_.word_str);
+                            message_.out_message.info = await wordlistdb.AddWordToWordBook(message_.in_message.wordbook_str, message_.in_message.word_str);
                             //////////////////////////////
                             //other implementations
                             //////////////////////////////
-                            return result_add == false ? "false" : "true";
+                            return; //result_add == false ? "false" : "true";
                         case METHOD_M_S.method_lookup_word:
-                            message_.out_message = await wordlistdb.GetWordDefinitionInWordBook(message_.wordbook_str, message_.word_str);
-                            return message_.out_message == null ? "false" : "true";
+                            message_.out_message.info = await wordlistdb.GetWordDefinitionInWordBook(message_.in_message.wordbook_str, message_.in_message.word_str);
+                            return; //message_.out_message.info == null ? "false" : "true";
+                        case METHOD_M_S.method_delete_word:
+                            message_.out_message.info = await wordlistdb.DeleteWordFromWordBook(message_.in_message.wordbook_str, message_.in_message.word_str);
+                            return;
                         default:
-                            return "method without definition";
+                            return; //"method without definition";
                     }
                     
                 case PARTICIPANT.M_U:
@@ -142,42 +219,22 @@ namespace MASTER_CONTROLL
                     {
 
                         case METHOD_M_U.method_show_definition:
-                            string result = await App1.MainPage.wakeUI(message_.in_message);
+
+                            App1.Popup instance = new App1.Popup();
+                            string result = await instance.wakeUI(message_.in_message.word_str);
                             //////////////////////////////
                             //other implementations
                             //////////////////////////////
-                            return result;
+                            return; //result;
                     
                         default:
-                            return "method without definition";
+                            return; //"method without definition";
                     }
-                    
                 default:
-                    return "participant without definition";
+                    return; //"participant without definition";
 
             }
         }
     }
 }
 
-/*
-namespace COMMASYNC
-{
-   public class PROGRAM
-    {
-       
-        public  async void addwordtowordbook_async(string wordbookid, string word)
-        {
-            WordListDB wordlistdb = new WordListDB();
-            bool result = await wordlistdb.AddWordToWordBook(wordbookid, word);
-            if (result)
-            {
-
-            }
-            else
-            {
-
-            }                
-        }
-    }
-}*/
